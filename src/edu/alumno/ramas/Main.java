c755e35f92b451993a7084905bff15a4cdcb5633
oleglukyy <lukianetsoleg@gmail.com>
2025-11-19 19:10:03 +0100

    Mensaje en consola (rama feature/mensaje)

--- src/edu/alumno/ramas/Main.java
+++ src/edu/alumno/ramas/Main.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+    	
         // Lotería Primitiva: 5 números principales (1..49) + 1 complementario
         HashSet<Integer> bolas = new HashSet<>();
         Random r = new Random();
@@ -13,7 +14,7 @@ public class Main {
         }
         System.out.println("Números principales: " + bolas);
         int complementario = r.nextInt(49) + 1;
-        System.out.println("Complementario: " + complementario);
+        System.out.println("¡Suerte en el sorteo, DAM!");
         int suma = bolas.stream().mapToInt(Integer::intValue).sum();
         System.out.println("Suma de principales: " + suma);
     }
